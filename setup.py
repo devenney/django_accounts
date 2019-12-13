eb7c2d09758997c62f97ec8872b2a929e1824cb6
Brendan Devenney <brendan.devenney@form3.tech>
2019-12-13 18:32:00 +0000

    Add description for PyPi packaging (#4)
    * Expand package metadata
    
    Signed-off-by: Brendan Devenney <brendan.devenney@form3.tech>
    
    * Pin Django to ~=2.0.0
    
    Signed-off-by: Brendan Devenney <brendan.devenney@form3.tech>
    
    * Ignore IDEA configuration
    
    Signed-off-by: Brendan Devenney <brendan.devenney@form3.tech>
    
    * Fix W605
    
    Signed-off-by: Brendan Devenney <brendan.devenney@form3.tech>

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
   name='devenney-accounts',
-  version='1.0.0',
+  version='1.0.1',
+  description='Elegant Django account management',
+  long_description=readme(),
   author='Brendan Devenney',
   author_email='brendan@devenney.io',
   packages=['devenney.accounts'],
@@ -11,8 +17,8 @@ setup(
   },
   package_data={'devenney.accounts':['templates/*/*.html','templates/*/*/*.html']},
   url='https://github.com/devenney/django_accounts',
-  license='LICENSE',
+  license='MIT',
   install_requires=[
     "Django >= 2.0"
   ],
-)
+
